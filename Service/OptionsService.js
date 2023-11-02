41d69ffc8c09331826ef27508bd7898dd2f4401f
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-11-02 15:41:46 -0300

    Função de inserir opções das perguntas

--- Service/OptionsService.js
+++ Service/OptionsService.js
@@ -1,4 +1,5 @@
 const Options = require('../Model/opcoes');
+const sequelize = require('../db');
 
 class OptionsService {
     getOptionsQuestions(data) {
@@ -12,7 +13,44 @@ class OptionsService {
     }
 
     insertOptions(data) {
-        console.log("Chegou: ",data);
+        const arrayOptions = data.options;
+        sequelize.transaction().then(async (t) => {
+            try {
+                await Options.create({
+                    idquestao: data.questionOptions,
+                    nivel: data.nivelOptions,
+                    correta: arrayOptions[0].correct1,
+                    opcao: arrayOptions[0].option1
+                }, { transaction: t });
+
+                await Options.create({
+                    idquestao: data.questionOptions,
+                    nivel: data.nivelOptions,
+                    correta: arrayOptions[1].correct2,
+                    opcao: arrayOptions[1].option2
+                }, { transaction: t });
+
+                await Options.create({
+                    idquestao: data.questionOptions,
+                    nivel: data.nivelOptions,
+                    correta: arrayOptions[2].correct3,
+                    opcao: arrayOptions[2].option3
+                }, { transaction: t });
+
+                await Options.create({
+                    idquestao: data.questionOptions,
+                    nivel: data.nivelOptions,
+                    correta: arrayOptions[3].correct4,
+                    opcao: arrayOptions[3].option4
+                }, { transaction: t });
+
+                await t.commit();
+
+            } catch (error) {
+                await t.rollback();
+                console.error('Erro durante a transação:', error);
+            }
+        })
     }
 }
 
