fe053a36584ba4ea6378acb63df90c37f854f63d
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-11-16 21:27:47 -0300

    implementação da contagem de pontuação do usuário

--- Service/PointsService.js
+++ Service/PointsService.js
@@ -1,8 +1,28 @@
-const PointsLevel = require('../Model/pontosnivel')
+const PointsLevel = require('../Model/pontosnivel');
 
 class PointsService {
-    async setPointsLevel(data){
-        console.log("Chegouuuuuu service", data)
+    async setPointsLevel(data) {
+        const checkPoints = await PointsLevel.findAll({
+            where: {
+                id: data.id,
+                nivel: data.nivel
+            }
+        })
+
+        if (checkPoints.length == 0) {
+            await PointsLevel.create({
+                id: data.id,
+                nivel: data.nivel,
+                pontos: data.pontos
+            })
+        } else {
+            await PointsLevel.update({pontos: data.pontos}, {
+                where: {
+                    id: data.id,
+                    nivel: data.nivel
+                }
+            })
+        }
     }
 }
 
