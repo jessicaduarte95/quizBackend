bd5bc8e97c0e1e6ae9ada087cd321f9b1102ac67
jessicaduarte95 <jessicaduarte.95@gmail.com>
2024-02-14 22:59:19 -0300

    funcionalidade para checar se o email existe

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -6,8 +6,10 @@ const userController = new UserController();
 
 const cadastrarUsuario = app.post('/cadastrar', userController.createUser);
 const login = app.post('/login', userController.login);
+const checkUser = app.post('/checkUser', userController.checkUser);
 
 module.exports = {
     cadastrarUsuario,
-    login
+    login,
+    checkUser
 };
