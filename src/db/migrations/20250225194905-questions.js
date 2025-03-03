cc261c50d64551ce1780de7c9cbffc907daeb8b0
Jessica Duarte <jessicaduarte.95@gmail.com>
2025-03-03 14:52:55 -0300

    ✨ Add data in question migrate

--- src/db/migrations/20250225194905-questions.js
+++ src/db/migrations/20250225194905-questions.js
@@ -209,6 +209,174 @@ module.exports = {
         "question": `Qual é o termo utilizado para descrever os eventos astronômicos que acontecem no final da vida de certas estrelas massivas, 
         marcados pela rápida liberação de uma grande quantidade de energia?`,
       },
+
+      //Level 5
+      {
+        "level": 5,
+        "question": `É um fenômeno astronômico que ocorre quando um fragmento de rocha espacial entra na atmosfera terrestre a uma velocidade extremamente alta, atingindo a superfície da Terra: `,
+      },
+      {
+        "level": 5,
+        "question": "Quais são os dois principais elementos que compõem o Sol?",
+      },
+      {
+        "level": 5,
+        "question": "Qual é o estado físico do sol?",
+      },
+      {
+        "level": 5,
+        "question": "Qual elemento químico é muito raro no planeta Terra, devido à sua baixa abundância na crosta terrestre?",
+      },
+      {
+        "level": 5,
+        "question": "A teoria da relatividade, mostra que o tempo não é absoluto, mas sim relativo, dependendo de quais fatores?",
+      },
+      {
+        "level": 5,
+        "question": "Quem foi responsável pelo desenvolvimento da mecânica clássica?",
+      },
+      {
+        "level": 5,
+        "question": "Qual elemento é utilizado para datar ossos em estudos arqueológicos e paleontológicos?",
+      },
+      {
+        "level": 5,
+        "question": "Componente do universo que constitui a maior parte da matéria total do universo, mas não emite, absorve ou reflete luz:",
+      },
+      {
+        "level": 5,
+        "question": "Qual é o símbolo químico do ouro na tabela periódica?",
+      },
+      {
+        "level": 5,
+        "question": "Qual é o nome do corpo celeste composto principalmente por gelo, poeira e rocha que orbita o Sol e forma uma cauda quando se aproxima do Sol?",
+      },
+
+      //Level 6
+      {
+        "level": 6,
+        "question": "Qual é o Telescópio que tem demonstrado grande evolução na atualidade?",
+      },
+      {
+        "level": 6,
+        "question": "O que é um grande sistema de estrelas, gás interestelar, poeira cósmica e matéria escura ligada pela gravidade?",
+      },
+      {
+        "level": 6,
+        "question": "O que são planetas que orbitam estrelas fora do nosso sistema solar?",
+      },
+      {
+        "level": 6,
+        "question": "O que é um objeto celeste composto principalmente de hidrogênio e hélio que brilha devido à fusão nuclear em seu núcleo?",
+      },
+      {
+        "level": 6,
+        "question": "O que é um corpo celeste que orbita um planeta, variando em tamanho, forma e composição?",
+      },
+      {
+        "level": 6,
+        "question": `O que  é uma hipotética estrutura no espaço-tempo que pode conectar duas regiões distantes do universo, teoricamente, poderiam servir como atalhos para viagens interestelares?`,
+      },
+      {
+        "level": 6,
+        "question": "Qual é o símbolo químico do hélio na tabela periódica?",
+      },
+      {
+        "level": 6,
+        "question": "Qual é o campo da ciência que busca entender a origem, evolução, distribuição e futuro da vida no universo?",
+      },
+      {
+        "level": 6,
+        "question": "Os viajantes espaciais dos Estados Unidos são chamados de astronautas. Da Rússia, eles são chamados de cosmonautas. De onde são os taikonautas?",
+      },
+      {
+        "level": 6,
+        "question": "É uma forma de matéria composta de antipartículas, que são partículas que possuem as mesmas massas, mas com cargas elétricas e outras propriedades físicas opostas:",
+      },
+
+      //Level 7
+      {
+        "level": 7,
+        "question": "Quantos países têm agências espaciais?",
+      },
+      {
+        "level": 7,
+        "question": "Qual é a principal fonte de produção do oxigênio na Terra?",
+      },
+      {
+        "level": 7,
+        "question": "Qual é o nome da maior lua de Júpiter?",
+      },
+      {
+        "level": 7,
+        "question": "É capaz de viajar mais rápido pela água do que pelo ar:",
+      },
+      {
+        "level": 7,
+        "question": "Qual é a porcentagem aproximada da superfície da Terra que é coberta por água?",
+      },
+      {
+        "level": 7,
+        "question": "Qual é o termo que pode ser definido como uma medida de desordem, aleatoriedade ou incerteza em um sistema?",
+      },
+      {
+        "level": 7,
+        "question": "Qual é o nome das partículas que possuem carga elétrica neutra e uma massa muito pequena?",
+      },
+      {
+        "level": 7,
+        "question": "Qual é o nome da partícula elementar que constitui a menor unidade de luz, sendo considerado o quantum (ou pacote) de energia?",
+      },
+      {
+        "level": 7,
+        "question": "Quando foi anunciada a descoberta do Bóson de Higgs?",
+      },
+      {
+        "level": 7,
+        "question": "Qual é o nome da teoria que busca explicar todas as forças fundamentais da natureza em um único quadro teórico coerente?",
+      },
+
+      //Level 8
+      {
+        "level": 8,
+        "question": "Qual é o nome do corpo celeste, decorrente de uma explosão de supernova, com um intenso campo magnético?",
+      },
+      {
+        "level": 8,
+        "question": "Qual é o nome do centro de um buraco negro, onde a luz é incapaz de sair?",
+      },
+      {
+        "level": 8,
+        "question": "Qual é a coloração das estrelas mais quentes do universo?",
+      },
+      {
+        "level": 8,
+        "question": "O que são pequenas partículas de matéria encontradas no espaço interestelar e intergaláctico?",
+      },
+      {
+        "level": 8,
+        "question": "Qual é o elemento mais abundante do universo?",
+      },
+      {
+        "level": 8,
+        "question": "Há quantos bilhões de anos, aproximadamente, a terra foi formada?",
+      },
+      {
+        "level": 8,
+        "question": "Quem é conhecido como o pai da física quântica?",
+      },
+      {
+        "level": 8,
+        "question": "Qual é o ramo da física que estuda as relações entre calor, trabalho e outras formas de energia em sistemas físicos?",
+      },
+      {
+        "level": 8,
+        "question": "Entre os planetas do nosso sistema solar, qual deles possui a rotação mais rápida em torno de seu próprio eixo?",
+      },
+      {
+        "level": 8,
+        "question": "A teoria conhecida como superstring (supercorda) propõe a existência de quantas dimensões espaciais?",
+      },
     ])
   },
 
